22913a9645627c738d882e21f00cfb3502af9d56
pranay-L53 <prajapatipranay@gmail.com>
2025-05-23 18:42:59 +0530

    Flutter Demo

---
+++ flutter_demo_1/lib/app/core/helper/app_strings.dart
@@ -0,0 +1,7 @@
+class AppString {
+  static const tussly = 'Tussly';
+  static const myPlayerCard = 'My Player Card';
+  static const myTeams = 'My Teams';
+  static const myTournaments = 'My Tournaments';
+  static const howTheTusslyArenaWorks = 'How the Tussly Arena Works';
+}
--- flutter_demo_1/lib/app/modules/initial/views/initial_view.dart
+++ flutter_demo_1/lib/app/modules/initial/views/initial_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_demo_1/app/core/helper/app_colors.dart';
+import 'package:flutter_demo_1/app/core/helper/app_strings.dart';
 import 'package:flutter_demo_1/app/core/helper/custom_style.dart';
+import 'package:flutter_demo_1/app/core/helper/images_resources.dart';
 import 'package:flutter_demo_1/app/widgets/custom_top_tabbar.dart';
 import 'package:get/get.dart';
 
@@ -18,65 +20,194 @@ class InitialView extends GetWidget<InitialController> {
             TopTabBar.header(
               // backPress: Get.toNamed(Routes.INITIAL),
               backPress: () => print("Tapped"),
-              title: 'Tussly',
+              title: AppString.tussly,
             ),
             SizedBox(height: 10),
             Expanded(
-              child: Column(
-                // mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    color: AppColors.colorThemeRed,
-                    height: 50,
-                    child: Row(
+              child: SingleChildScrollView(
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: AppColors.colorThemeRed,
+                      height: 50,
+                      child: Row(
+                        children: [
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: Text(
+                              'Register for free',
+                              style: AppTextStyles.themeBoldStyle(
+                                fontSize: 17.0,
+                                fontColor: AppColors.colorWhite,
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: AppColors.colorWhite,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(13.5),
+                              ),
+                            ),
+                            width: 90.0,
+                            height: 27,
+                            child: Center(
+                              child: Text(
+                                'Login',
+                                style: AppTextStyles.themeMediumStyle(
+                                  fontSize: 14.0,
+                                  fontColor: AppColors.colorBlack,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: AppColors.colorWhite,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(13.5),
+                              ),
+                            ),
+                            width: 90.0,
+                            height: 27,
+                            child: Center(
+                              child: Text(
+                                'Sign Up',
+                                style: AppTextStyles.themeMediumStyle(
+                                  fontSize: 14.0,
+                                  fontColor: AppColors.colorBlack,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                        ],
+                      ),
+                    ),
+                    Row(
                       children: [
-                        SizedBox(width: 10),
-                        Expanded(
-                          child: Text(
-                            'Register for free',
-                            style: AppTextStyles.themeBoldStyle(
-                              fontSize: 17.0,
-                              fontColor: AppColors.colorWhite,
-                            ),
-                          ),
-                        ),
-                        SizedBox(width: 10),
                         Container(
-                          color: AppColors.colorWhite,
-                          width: 90.0,
-                          height: 30,
-                          child: Center(
-                            child: Text(
-                              'Login',
-                              style: AppTextStyles.themeMediumStyle(
-                                fontSize: 13.0,
-                                fontColor: AppColors.colorBlack,
+                          width: Get.width / 2,
+                          padding: EdgeInsets.only(
+                            left: 10,
+                            right: 5,
+                            top: 10,
+                            bottom: 10,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                AppString.myPlayerCard,
+                                style: AppTextStyles.themeBoldStyle(
+                                  fontSize: 17.0,
+                                  fontColor: AppColors.colorBlack,
+                                ),
                               ),
-                            ),
+                              ClipOval(
+                                child: Image.asset(
+                                  ImageResources.swift,
+                                  height: (Get.width / 2) - 15,
+                                  color: AppColors.colorThemeRed,
+                                ),
+                              ),
+                              Text(AppString.myPlayerCard),
+                            ],
                           ),
                         ),
-                        SizedBox(width: 10),
                         Container(
-                          color: AppColors.colorWhite,
-                          width: 90.0,
-                          height: 30,
-                          child: Center(
-                            child: Text(
-                              'Sign Up',
-                              style: AppTextStyles.themeMediumStyle(
-                                fontSize: 13.0,
-                                fontColor: AppColors.colorBlack,
+                          width: Get.width / 2,
+                          padding: EdgeInsets.only(
+                            left: 5,
+                            right: 10,
+                            top: 10,
+                            bottom: 10,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                AppString.myTeams,
+                                style: AppTextStyles.themeBoldStyle(
+                                  fontSize: 17.0,
+                                  fontColor: AppColors.colorBlack,
+                                ),
                               ),
-                            ),
+                              ClipOval(
+                                child: Image.asset(
+                                  ImageResources.swift,
+                                  height: (Get.width / 2) - 15,
+                                  color: AppColors.colorThemeRed,
+                                ),
+                              ),
+                              Text(AppString.myTeams),
+                            ],
                           ),
                         ),
-                        SizedBox(width: 10),
                       ],
                     ),
-                  ),
-                ],
+                    SizedBox(height: 5),
+                    Container(
+                      width: Get.width - 20,
+                      // height: (Get.width / 2) - 20,
+                      padding: EdgeInsets.only(left: 10, right: 5, bottom: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            AppString.myPlayerCard,
+                            style: AppTextStyles.themeBoldStyle(
+                              fontSize: 17.0,
+                              fontColor: AppColors.colorBlack,
+                            ),
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(30.0),
+                            ), // match the outer border
+                            child: Image.asset(
+                              ImageResources.swift,
+                              height: (Get.width / 2) - 10,
+                              width: Get.width,
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection:
+                          Axis.horizontal, // 👈 horizontal scrolling
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 8.0,
+                          ), // optional spacing
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                ImageResources.swift,
+                                width: 150.0,
+                                height: 150.0,
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                      itemCount: 5,
+                    ),
+                  ],
+                ),
               ),
             ),
+            SizedBox(height: 10),
             TopTabBar.footer(
               context: context,
               backPress: () => print("Tapped"),
