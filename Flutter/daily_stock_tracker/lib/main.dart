b92d3291e2ebfeb13c2f6b1480c0c4a9213d0728
Pranay <105724674+pranayAuxano@users.noreply.github.com>
2025-12-11 12:23:57 +0530

    Working on App Storage.

--- Flutter/daily_stock_tracker/lib/main.dart
+++ Flutter/daily_stock_tracker/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'package:get_storage/get_storage.dart';
 import 'app/routes/app_pages.dart';
 
-void main() {
-  runApp(
+void main() async {
+  /*runApp(
     GetMaterialApp(
       title: "Daily Usage",
       initialRoute: AppPages.INITIAL,
@@ -12,5 +12,23 @@ void main() {
       // builder: EasyLoading.init(),
       debugShowCheckedModeBanner: false,
     ),
-  );
+  );*/
+
+  // Initialize GetStorage BEFORE runApp()
+  await GetStorage.init();
+
+  runApp(MyApp());
+}
+
+/// Separate App widget
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "Daily Usage",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
